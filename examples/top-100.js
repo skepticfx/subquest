c932588e7d0e9e3a3b0ef82ed8982c0185630a68
b4dnewz <filippo@codekraft.it>
2017-10-21 23:37:37 +0200

    Updated example

--- examples/top-100.js
+++ examples/top-100.js
@@ -1,7 +1,12 @@
-// top-100.js
+'use strict';
 
-// var subquest = require('subquest');
 var subquest = require('../');
+var domain = 'google.com'
 
-console.log('Here are the top sub-domains for google.com');
-subquest.find({domain: 'google.com'});
+console.log('Here are the top sub-domains for:', domain);
+
+subquest.getSubDomains({
+  host: domain
+}).on('end', function(arr){
+  console.log(arr);
+});
