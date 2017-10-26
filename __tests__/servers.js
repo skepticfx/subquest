d7c730103ffd4619b0a9fda1de6b3ba573f0788c
Filippo Conti <filippo@codekraft.it>
2017-10-26 14:06:35 +0200

    Changed tests order
    This hopefully will fix the multiple calls of setServers which easily bugs

--- __tests__/servers.js
+++ __tests__/servers.js
@@ -6,6 +6,19 @@ describe('DNS Servers', function() {
   beforeAll(function() {
     jest.setTimeout(5000)
   })
+  
+  describe('adds a user defined DNS server to the stack', function() {
+    it('returns user defined DNS server as first', function(done) {
+      // Custom DNS server
+      let customDNS = '91.239.100.100'
+  
+      // Get resolvers list by adding valid provided server
+      subquest.getResolvers(customDNS, function(result) {
+        expect(result[0]).toBe(customDNS)
+        done()
+      })
+    })
+  })
 
   describe('test valid DNS server before adding to resolvers', function() {
   
